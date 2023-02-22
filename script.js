090a4ab42cde3832489558d68f4a6184df25c4e1
Paulo Amarante <pcfdaa@gmail.com>
2023-02-22 17:48:50 -0300

    projeto niver

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function checkButton() {
     const form = document.getElementById("formulario");
     const button = document.getElementById("enviar");
     form.classList.add("aparecer");
+    button.classList.add("aparecer");   
     setTimeout(() => {
         form.classList.add("ativo");
+        button.classList.add("ativo")
     }, 100);
-    button.classList.remove("escondido");
-    form.classList.remove("escondido");
 };
